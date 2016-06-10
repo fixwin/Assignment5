42de14d562388a6b99cb938da8bdf95b34854094
yonatan <yo.pearlmutter@gmail.com>
2016-06-10 11:29:49 +0300

    heaps

---
+++ src/MaxHeap.java
@@ -0,0 +1,76 @@
+public class MaxHeap {
+	public Point heap[] = new Point[10000];
+	public int n = 0, child, parent, child1, child2;
+
+	public void insert(Point value){
+		heap[n] = value; // insert the new value in the last position of the array
+
+		parent = (n+1)/2 - 1;
+		if(parent < 0) {n++; return;} // if the only node present is the root, then return immediately
+		child = n;
+
+		// do bubble up if there are more than two nodes in the tree
+		while(true){
+
+			// only need to compare newly inserted node and its parent
+			if(heap[child].getY() > heap[parent].getY()){
+
+				// swap the child and the parent if the child is greater than parent in case of max heap
+				Point temp = heap[child];
+				heap[child] = heap[parent];
+				heap[parent] = temp;
+
+				// stop bubble up when the root is reached
+				if(parent == 0) break;
+
+				// update the child and parent. The previous parent becomes the new child
+				child = parent;
+				parent = (parent + 1)/2 - 1;
+			}
+			else break;
+		}
+		n++;
+	}
+
+	// function which deletes the max element(or root) of the heap
+	public Point delete(){
+
+		// bring the last element to the first position of the array
+		Point ele = heap[0];
+		heap[0] = heap[n-1];
+
+		parent = 0;
+
+		while(true){
+			child1 = parent*2 + 1;
+			child2 = parent*2 + 2;
+
+			// condition to stop bubble down
+			if(child1 > n-1 || child2 > n-1) break;
+
+			// swap the parent with the largest of its two children
+			if(heap[parent].getY() < heap[child1].getY() || heap[parent].getY() < heap[child2].getY()){
+				if(heap[child1].getY() > heap[child2].getY()){
+					Point temp = heap[child1];
+					heap[child1] = heap[parent];
+					heap[parent] = temp;
+					parent = child1; // update the new parent
+				}
+				else{
+					Point temp = heap[child2];
+					heap[child2] = heap[parent];
+					heap[parent] = temp;
+					parent = child2; // update the new parent
+				}
+			}
+			else break;
+		}
+		n--;
+		return ele; // return the max element that was deleted from the max heap(the root)
+	}
+
+	// function to simply return the max element of the heap(or root) without deleting it
+	public Point getMax(){
+		return heap[0];
+	}
+}
---
+++ src/MinHeap.java
@@ -0,0 +1,68 @@
+public class MinHeap {
+	public Point heap[] = new Point[10000];
+	public int n, child, parent, child1, child2;
+
+	public void insert(Point value){
+		heap[n] = value; // insert the new value into the end of the array
+
+		parent = (n+1)/2 - 1;
+		if(parent < 0) {n++; return;} // if the heap contains only one node, then return immediately
+		child = n;
+
+		// bubble up
+		while(true){
+			if(heap[child].getY() < heap[parent].getY()){
+				Point temp = heap[child];
+				heap[child] = heap[parent];
+				heap[parent] = temp;
+				if(parent == 0) break;
+				child = parent;
+				parent = (parent + 1)/2 - 1;
+			}
+			else break;
+		}
+		n++;
+	}
+
+	public Point delete(){
+
+		// bring the last element of the array into the first position
+		Point ele = heap[0];
+		heap[0] = heap[n-1];
+
+		parent = 0;
+
+		// bubble down
+		while(true){
+			child1 = parent*2 + 1;
+			child2 = parent*2 + 2;
+
+			// condition for when to stop bubble down
+			if(child1 > n-1 || child2 > n-1) break;
+
+			// swap the parent with the least of its children in the case of min heap
+			if(heap[parent].getY() > heap[child1].getY() || heap[parent].getY() > heap[child2].getY()){
+				if(heap[child1].getY() < heap[child2].getY()){
+					Point temp = heap[child1];
+					heap[child1] = heap[parent];
+					heap[parent] = temp;
+					parent = child1;
+				}
+				else{
+					Point temp = heap[child2];
+					heap[child2] = heap[parent];
+					heap[parent] = temp;
+					parent = child2;
+				}
+			}
+			else break;
+		}
+		n--;
+		return ele; // return the root(or min) element of the heap
+	}
+
+	// function to simply return the min element(or root) of the heap without deleting it
+	public Point getMin(){
+		return heap[0];
+	}
+}
